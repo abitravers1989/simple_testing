95c0f1e393cb99e399be41162ae3ea42752b24f1
abitravers1989 <atravers4007@gmail.com>
2018-10-30 19:22:15 +0000

    First test

---
+++ test/bmi.test.js
@@ -0,0 +1,17 @@
+const bmi = require('../bmi')
+const assert = require('assert');
+
+describe('BMI', () => {
+    it('should return 24.9 when given a height of 1.90 and weight of 90k', () => {
+        //Arrange 
+        const height = 1.9;
+        const weight = 90;
+        const expectedResult = 90;
+
+        //Act
+        const calBMI = bmi.calculateBMI(height, weight)
+
+        //Assert
+        assert.Equal(calBMI, expectedResult)
+    })
+})
