275ce7852ce0c624aae1be0c843dc4496c2e84ba
abitravers1989 <atravers4007@gmail.com>
2018-10-31 10:04:47 +0000

    adding sinon spy tests

---
+++ test/userAcct.test.js
@@ -0,0 +1,34 @@
+const userAccount = require('../userAcct')
+const { should, expect } = require('chai')
+const sinon = require('sinon');
+
+
+//We want to know more this time so as we can see how many times the getsData method has been called on this userAccountsService
+describe("userAccount", () => {
+    let sandbox;
+
+    beforeEach(() => {
+        console.log(sandbox)
+        sandbox = sinon.createSandbox();
+    })
+
+    afterEach(() => {
+        console.log(sandbox)
+        sandbox.restore()
+    })
+    it('When userAccount is given two emails getData is called twice ', () => {
+        //Arrange 
+        const spy = sandbox.spy(getData)
+        const email1 = "test@test.com"
+        const email2 = "testtwo@test.com"
+
+        //Act
+        const userAccount = userAccount(email1, email2)
+
+        //Assert
+        assert(spy.calledWith(email1));
+        expect(spy.getCall(2))
+        expect(spy).toHaveBeenCalled();
+        expect(spy.callCount).toEqual(2)
+    })
+})
