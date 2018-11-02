9749be8d291ee7e6c6b35108f685cb933761c009
abitravers1989 <atravers4007@gmail.com>
2018-11-02 15:46:14 +0000

    add promise understanding

--- promisePractice.js
+++ promisePractice.js
@@ -39,7 +39,7 @@ higherOrderFunction(10, add)
 //how do you listen for when the status of a promise changes?
 
 //create
-const promise = new Promise()
+//const promise = new Promise()
 
 //change the status
 const promise2 = new Promise((resolve, reject) => {
@@ -48,3 +48,106 @@ const promise2 = new Promise((resolve, reject) => {
     //reject changes it to rejected.
     reject()
 })
+
+console.log(Promise.then);
+
+//when status of promise changes to fulfiled then status will be passed to then
+//when status is changed to rejected it is passed to catch. the function that you pass to .catch
+
+function onSuccess() {
+    console.log('success')
+}
+
+function onError() {
+    console.log('failed')
+}
+
+const promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve() //changes status to fulfilled
+    }, 2000)
+})
+
+promise.then(onSuccess)
+promise.catch(onError)
+
+const promise3 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        reject() //changes status to rejected
+    }, 2000)
+})
+
+promise3.then(onSuccess)
+promise3.catch(onError)
+
+
+
+//wrapping JQuery (sick emoji) in promises 
+
+function getUser(id) {
+    return new Promise = function (resolve, reject) {
+        $.getJSON({
+            url: `https://api.github.com/users/${id}`,
+            success: resolve("you can pass stuff here which you can access"),
+            error: reject,
+        })
+    }
+}
+
+function getWeather(user) {
+    return new Promise = ((resolve, reject) => {
+        $.getJSON({
+            url: getLocationURL(user.location.split(',')),
+            success: resolve,
+            error: reject,
+        })
+    })
+}
+
+$("#btn").on("click", () => {
+    const userPromise = getUser('userznames')
+
+    userPromise.then((user) => {
+        const weatherPromise = getWeather(user)
+        weatherPromise.then((weather) => {
+            //do something
+        })
+        weatherPromise.catch(showError)
+    })
+    userPromise.catch(showError)
+})
+
+//imporved 
+
+function getUser(id) {
+    return new Promise = function (resolve, reject) {
+        $.getJSON({
+            url: `https://api.github.com/users/${id}`,
+            success: resolve("you can pass stuff here which you can access"),
+            error: reject,
+        })
+    }
+}
+
+function getWeather(user) {
+    return new Promise = ((resolve, reject) => {
+        $.getJSON({
+            url: getLocationURL(user.location.split(',')),
+            success: resolve,
+            error: reject,
+        })
+    })
+}
+
+$("#btn").on("click", () => {
+    getUser('userznames')
+        .then(getWeather)
+        .then((wether) => {
+            updateUI()
+        })
+        .catch(showError)
+})
+
+
+
+//https://tylermcginnis.com/async-javascript-from-callbacks-to-promises-to-async-await/
