dcdc5c8d678d401bad12b7267807259fc046a647
abitravers1989 <atravers4007@gmail.com>
2018-11-07 16:34:53 +0000

    test passing

--- userAcct2.js
+++ userAcct2.js
@@ -1,4 +1,21 @@
 module.exports = class UserAccountService {
-    getData() { n += 1 }
-    fetchData() { }
+    fetchData() {
+        try {
+            var getUser = this.getData();
+            var getAccount = this.getData();
+            var getBalance = this.getData();
+        } catch (e) {
+            return new Error("data issue")
+        }
+
+        if (getUser && getAccount && getBalance) {
+            return true;
+        }
+
+        return false;
+    }
+
+    getData() {
+        return true;
+    }
 }
